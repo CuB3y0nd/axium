fd1747ed10d51045ee6489e5bc74f10d149d050d
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2026-01-04 01:39:48 +0800

    fix: check if exec failed

--- src/axium/tubes/process.c
+++ src/axium/tubes/process.c
@@ -1,4 +1,5 @@
 #include <axium/tubes/process.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,6 +11,7 @@ process(char *const argv[], char *const envp[]) {
   int p2c[2] = {-1, -1};
   int c2p[2] = {-1, -1};
   int e2p[2] = {-1, -1};
+  int sync_pipe[2] = {-1, -1};
   pid_t pid;
   tube *t;
 
@@ -34,6 +36,11 @@ process(char *const argv[], char *const envp[]) {
     goto fail;
   }
 
+  if (pipe2(sync_pipe, O_CLOEXEC) == -1) {
+    perror("pipe2 (sync_pipe)");
+    goto fail;
+  }
+
   pid = fork();
   if (pid == -1) {
     perror("fork");
@@ -41,9 +48,11 @@ process(char *const argv[], char *const envp[]) {
   }
 
   if (pid == 0) {
+    close(sync_pipe[0]);
     if (dup2(p2c[0], STDIN_FILENO) == -1 || dup2(c2p[1], STDOUT_FILENO) == -1 ||
         dup2(e2p[1], STDERR_FILENO) == -1) {
-      perror("dup2");
+      int err = errno;
+      write(sync_pipe[1], &err, sizeof(err));
       _exit(EXIT_FAILURE);
     }
 
@@ -53,8 +62,30 @@ process(char *const argv[], char *const envp[]) {
       execvp(argv[0], argv);
     }
 
-    perror("exec");
+    int err = errno;
+    write(sync_pipe[1], &err, sizeof(err));
     _exit(EXIT_FAILURE);
+  }
+
+  close(sync_pipe[1]);
+  sync_pipe[1] = -1;
+  int err;
+  ssize_t n = read(sync_pipe[0], &err, sizeof(err));
+  close(sync_pipe[0]);
+  sync_pipe[0] = -1;
+
+  if (n > 0) {
+    // Child sent an error
+    if (n == sizeof(err)) { // ensure we get the integral errno code
+      errno = err;
+    }
+    perror("process: exec failed");
+    waitpid(pid, NULL, 0);
+    goto fail;
+  } else if (n == -1) {
+    perror("read (sync_pipe)");
+    waitpid(pid, NULL, 0);
+    goto fail;
   }
 
   close(p2c[0]);
@@ -80,6 +111,12 @@ fail:
   if (e2p[0] != -1) {
     close(e2p[0]);
     close(e2p[1]);
+  }
+  if (sync_pipe[0] != -1) {
+    close(sync_pipe[0]);
+  }
+  if (sync_pipe[1] != -1) {
+    close(sync_pipe[1]);
   }
   free(t);
   return NULL;
