1d338370666a573cfe12e4452d4cb1aebc164985
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2026-01-04 02:07:22 +0800

    test: fix test case for testing process

--- tests/axium/tubes/test_process.c
+++ tests/axium/tubes/test_process.c
@@ -14,33 +14,16 @@ int main(void) {
   char *const cat_argv[] = {"/bin/cat", NULL};
   tube *t2 = process(cat_argv, NULL);
   ASSERT_TRUE(t2 != NULL, "Create process for cat");
+  ASSERT_TRUE(t_out(t2) == -1, "Stdout should not be captured");
+  ASSERT_TRUE(t_err(t2) == -1, "Stderr should not be captured");
 
-  const char *test_input = "This is a test.\n";
+  const char *test_input = "This is a test input sent to cat's stdin.\n";
   ssize_t written_bytes = write(t_in(t2), test_input, strlen(test_input));
   ASSERT_TRUE(written_bytes == (ssize_t)strlen(test_input),
               "Write all bytes to stdin");
 
   // Close stdin to signal EOF to cat
   t_cin(t2);
-
-  char buffer[1024];
-  ssize_t read_bytes;
-  char received_output[1024] = {0};
-  size_t total_read = 0;
-
-  while ((read_bytes = read(t_out(t2), buffer, sizeof(buffer) - 1)) > 0) {
-    if (total_read + (size_t)read_bytes >= sizeof(received_output)) {
-      fprintf(stderr, "Buffer too small for received output.\n");
-      break;
-    }
-    memcpy(received_output + total_read, buffer, read_bytes);
-    total_read += read_bytes;
-  }
-  received_output[total_read] = '\0';
-
-  ASSERT_STREQ(received_output, test_input,
-               "Received output matches sent input");
-
   t_close(t2);
 
   return 0;
