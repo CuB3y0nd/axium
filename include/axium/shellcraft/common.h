9b5b6ef3431ed96d49de550988363925995b95c4
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2026-01-06 18:33:31 +0800

    feat: implement shellcode helper macros

---
+++ include/axium/shellcraft/common.h
@@ -0,0 +1,41 @@
+#ifndef AXIUM_SHELLCRAFT_COMMON_H
+#define AXIUM_SHELLCRAFT_COMMON_H
+
+/** @brief Attribute for naked functions (no prologue/epilogue) */
+#define NAKED __attribute__((naked))
+
+/**
+ * @brief Define shellcode boundaries for a naked function.
+ * @param name Function name.
+ */
+#define DEFINE_SHELLCODE(name)                                                 \
+  extern char name##_start[];                                                  \
+  extern char name##_end[];                                                    \
+  NAKED void name(void)
+
+/**
+ * @brief Inline assembly markers for the start of shellcode.
+ * @param name Function name.
+ */
+#define SHELLCODE_START(name)                                                  \
+  __asm__ volatile(".intel_syntax noprefix\n"                                  \
+                   ".global " #name "_start\n"                                 \
+                   ".global " #name "_end\n" #name "_start:\n")
+
+/**
+ * @brief Inline assembly markers for the end of shellcode.
+ * @param name Function name.
+ */
+#define SHELLCODE_END(name)                                                    \
+  __asm__ volatile(#name "_end:\n"                                             \
+                         ".att_syntax\n")
+
+/**
+ * @brief Append shellcode defined via DEFINE_SHELLCODE to a payload.
+ * @param p Pointer to the payload.
+ * @param name Name of the shellcode function.
+ */
+#define PAYLOAD_PUSH_SC(p, name)                                               \
+  payload_push(p, &name##_start, (size_t)(name##_end - name##_start))
+
+#endif // AXIUM_SHELLCRAFT_COMMON_H
