2cf4f532f733045543e45b5b65d3672723613fba
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2026-01-07 01:17:01 +0800

    feat: add global header file

---
+++ include/axium/axium.h
@@ -0,0 +1,37 @@
+#ifndef AXIUM_H
+#define AXIUM_H
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+/* Common standard headers */
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Axium modules */
+#include <axium/log.h>
+#include <axium/shellcraft/common.h>
+#include <axium/timeout.h>
+#include <axium/tubes/process.h>
+#include <axium/tubes/tube.h>
+#include <axium/utils/fiddling.h>
+#include <axium/utils/packing.h>
+#include <axium/utils/payload.h>
+#include <axium/utils/proc.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AXIUM_H */
