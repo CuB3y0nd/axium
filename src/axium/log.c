cc9f8bc9e5961c0033df532dc69329798737b502
CuB3y0nd <91041570+CuB3y0nd@users.noreply.github.com>
2026-01-07 15:33:02 +0800

    feat: log_status support

--- src/axium/log.c
+++ src/axium/log.c
@@ -1,20 +1,30 @@
 #include <axium/log.h>
+#include <axium/timeout.h>
 #include <errno.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 static log_level_t current_log_level = INFO;
+static bool is_status_active = false;
+static double last_status_time = 0;
+#define STATUS_THROTTLE_RATE 0.1
 
 void set_log_level(log_level_t level) { current_log_level = level; }
 
 log_level_t get_log_level(void) { return current_log_level; }
 
 static void vlog(log_level_t level, const char *prefix, const char *color,
-                 const char *fmt, va_list args) {
+                 bool is_status, const char *fmt, va_list args) {
   if (level < current_log_level) {
     return;
+  }
+
+  if (is_status_active) {
+    fprintf(stderr, "\r\x1b[K"); // Move to start of line and clear it
+    is_status_active = false;
   }
 
   if (color) {
@@ -22,42 +32,49 @@ static void vlog(log_level_t level, const char *prefix, const char *color,
   } else {
     fprintf(stderr, "[%s] ", prefix);
   }
+
   vfprintf(stderr, fmt, args);
-  fprintf(stderr, "\n");
+
+  if (is_status) {
+    is_status_active = true;
+    fflush(stderr);
+  } else {
+    fprintf(stderr, "\n");
+  }
 }
 
 void log_success(const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
-  vlog(INFO, "+", ANSI_BOLD_GREEN, fmt, args);
+  vlog(INFO, "+", ANSI_BOLD_GREEN, false, fmt, args);
   va_end(args);
 }
 
 void log_failure(const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
-  vlog(INFO, "-", ANSI_BOLD_RED, fmt, args);
+  vlog(INFO, "-", ANSI_BOLD_RED, false, fmt, args);
   va_end(args);
 }
 
 void log_info(const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
-  vlog(INFO, "*", ANSI_BOLD_BLUE, fmt, args);
+  vlog(INFO, "*", ANSI_BOLD_BLUE, false, fmt, args);
   va_end(args);
 }
 
 void log_warning(const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
-  vlog(WARNING, "!", ANSI_BOLD_YELLOW, fmt, args);
+  vlog(WARNING, "!", ANSI_BOLD_YELLOW, false, fmt, args);
   va_end(args);
 }
 
 void log_error(const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
-  vlog(ERROR, "ERROR", ANSI_ON_RED ANSI_WHITE, fmt, args);
+  vlog(ERROR, "ERROR", ANSI_ON_RED ANSI_WHITE, false, fmt, args);
   va_end(args);
   exit(EXIT_FAILURE);
 }
@@ -75,20 +92,26 @@ void log_exception(const char *fmt, ...) {
 void log_critical(const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
-  vlog(CRITICAL, "CRITICAL", ANSI_ON_RED ANSI_WHITE, fmt, args);
+  vlog(CRITICAL, "CRITICAL", ANSI_ON_RED ANSI_WHITE, false, fmt, args);
   va_end(args);
 }
 
 void log_debug(const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
-  vlog(DEBUG, "DEBUG", ANSI_BOLD_RED, fmt, args);
+  vlog(DEBUG, "DEBUG", ANSI_BOLD_RED, false, fmt, args);
   va_end(args);
 }
 
 void log_status(const char *fmt, ...) {
+  double now = timeout_now();
+  if (is_status_active && now - last_status_time < STATUS_THROTTLE_RATE) {
+    return;
+  }
+  last_status_time = now;
+
   va_list args;
   va_start(args, fmt);
-  vlog(INFO, "x", ANSI_MAGENTA, fmt, args);
+  vlog(INFO, "x", ANSI_MAGENTA, true, fmt, args);
   va_end(args);
 }
